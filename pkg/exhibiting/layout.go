620f4fc8976c837c099314eaa800330b52fe6c74
KY <me@ykaseng.com>
2021-10-15 23:10:46 +0800

    Add new layout

--- pkg/exhibiting/layout.go
+++ pkg/exhibiting/layout.go
@@ -10,24 +10,28 @@ const (
 	Default Layout = iota
 	Ultrawide
 	Split
+	Explode
 )
 
 var lmM = map[Layout]string{
 	Default:   "960x1080+0+0 1920x1080+960+0 960x1080+2880+0",
 	Ultrawide: "640x1080+0+0 2560x1080+640+0 640x1080+3200+0",
 	Split:     "1920x1080+0+0 1920x1080+1920+0",
+	Explode:   "1280x540+0+0 1280x540+1280+0 1280x540+2560+0 1280x540+0+540 1280x540+1280+540 1280x540+2560+540",
 }
 
 var slM = map[string]Layout{
 	"default":   Default,
 	"ultrawide": Ultrawide,
 	"split":     Split,
+	"explode":   Explode,
 }
 
 var lsM = map[Layout]string{
 	Default:   "Default",
 	Ultrawide: "Ultrawide",
 	Split:     "Split",
+	Explode:   "Explode",
 }
 
 func (l Layout) String() string {
--- pkg/observing/layout.go
+++ pkg/observing/layout.go
@@ -6,18 +6,21 @@ const (
 	Default Layout = iota
 	Ultrawide
 	Split
+	Explode
 )
 
 var mlM = map[string]Layout{
-	"960x1080+0+0 1920x1080+960+0 960x1080+2880+0": Default,
-	"640x1080+0+0 2560x1080+640+0 640x1080+3200+0": Ultrawide,
-	"1920x1080+0+0 1920x1080+1920+0":               Split,
+	"960x1080+0+0 1920x1080+960+0 960x1080+2880+0":                                                    Default,
+	"640x1080+0+0 2560x1080+640+0 640x1080+3200+0":                                                    Ultrawide,
+	"1920x1080+0+0 1920x1080+1920+0":                                                                  Split,
+	"1280x540+0+0 1280x540+1280+0 1280x540+2560+0 1280x540+0+540 1280x540+1280+540 1280x540+2560+540": Explode,
 }
 
 var lsM = map[Layout]string{
 	Default:   "Default",
 	Ultrawide: "Ultrawide",
 	Split:     "Split",
+	Explode:   "Explode",
 }
 
 func (l Layout) String() string {
