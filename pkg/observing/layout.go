50a1d402958cbc00dd4ee20f1ae739381053e5c0
KY <me@ykaseng.com>
2021-10-20 15:55:36 +0800

    Add new layouts

--- pkg/observing/layout.go
+++ pkg/observing/layout.go
@@ -4,23 +4,29 @@ type Layout int
 
 const (
 	Default Layout = iota
-	Ultrawide
 	Split
+	Column
+	Ultrawide
+	Fullscreen
 	Explode
 )
 
 var mlM = map[string]Layout{
-	"960x1080+0+0 1920x1080+960+0 960x1080+2880+0":                                                    Default,
-	"640x1080+0+0 2560x1080+640+0 640x1080+3200+0":                                                    Ultrawide,
-	"1920x1080+0+0 1920x1080+1920+0":                                                                  Split,
+	"960x1080+0+0 1920x1080+960+0 960x1080+2880+0":    Default,
+	"1920x1080+0+0 1920x1080+1920+0":                  Split,
+	"1280x1080+0+0 1280x1080+1280+0 1280x1080+2560+0": Column,
+	"640x1080+0+0 2560x1080+640+0 640x1080+3200+0":    Ultrawide,
+	"3840x1080+0+0": Fullscreen,
 	"1280x540+0+0 1280x540+1280+0 1280x540+2560+0 1280x540+0+540 1280x540+1280+540 1280x540+2560+540": Explode,
 }
 
 var lsM = map[Layout]string{
-	Default:   "Default",
-	Ultrawide: "Ultrawide",
-	Split:     "Split",
-	Explode:   "Explode",
+	Default:    "Default",
+	Split:      "Split",
+	Column:     "Column",
+	Ultrawide:  "Ultrawide",
+	Fullscreen: "Fullscreen",
+	Explode:    "Explode",
 }
 
 func (l Layout) String() string {
